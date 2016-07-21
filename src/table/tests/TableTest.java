8332138ae487ee40503d078f8bdbd76604636f3a
Shryne <enomeral@gmx.de>
2016-07-21 15:17:04 +0200

    Added the empty methods for the tests to be created.

---
+++ src/table/tests/TableTest.java
@@ -0,0 +1,103 @@
+package table.tests;
+
+import org.junit.Test;
+
+/**
+ * Contains all tests for the Table class.
+ */
+class TableTest {
+    @Test
+    public void emptyTable() {
+
+    }
+
+    @Test
+    public void noHeadLineOneData() {
+
+    }
+
+    @Test
+    public void oneHeadLineNoData() {
+
+    }
+
+    @Test
+    public void oneHeadLineOneData() {
+
+    }
+
+    @Test
+    public void bottomHeadline() {
+
+    }
+
+    @Test
+    public void rightHeadLine() {
+
+    }
+
+    @Test
+    public void leftHeadLine() {
+
+    }
+
+    @Test
+    public void allHeadLinesNoData() {
+
+    }
+
+    @Test
+    public void oneRow() {
+
+    }
+
+    @Test
+    public void multipleRows() {
+
+    }
+
+    @Test
+    public void oneRowWidth() {
+
+    }
+
+    @Test
+    public void multipleRowsWidth() {
+
+    }
+
+    @Test
+    public void oneRowAlignment() {
+
+    }
+
+    @Test
+    public void headLineAlignment() {
+
+    }
+
+    @Test
+    public void headLineDataWidth() {
+
+    }
+
+    @Test
+    public void headLineDataAlignment() {
+
+    }
+
+    @Test
+    public void multipleHeadlinesRowsWidth() {
+
+    }
+
+    @Test
+    public void multipleHeadlinesRowsAlignment() {
+
+    }
+
+    @Test
+    public void multipleHeadlinesRowsWidthAlignment() {
+
+    }
+}
